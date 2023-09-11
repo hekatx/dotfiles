f430a24f5bc17d0dd335a0747b45f41a7bcd5c73
eka <phyzheye01@gmail.com>
2023-09-11 14:03:46 -0500

    added diagnostics from eslint

--- nvim/.config/nvim/lua/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/lsp/null-ls.lua
@@ -4,18 +4,18 @@ local b = null_ls.builtins
 local M = {}
 
 M.setup = function(on_attach)
-  null_ls.setup({
-      sources = {
-          -- JS/TS
-          b.formatting.prettierd,
-          b.formatting.eslint_d,
-          b.diagnostics.eslint_d,
-          b.formatting.stylua,
-      },
-      on_attach = on_attach,
-      debug = true,
-      diagnostics_format = "#{m} (#{s}) ([#{c}])",
-  })
+	null_ls.setup({
+		sources = {
+			-- JS/TS
+			b.formatting.prettierd,
+			b.formatting.eslint_d,
+			b.diagnostics.eslint_d,
+			b.formatting.stylua,
+		},
+		on_attach = on_attach,
+		debug = true,
+		diagnostics_format = "#{m} (#{s}) ([#{c}])",
+	})
 end
 
 return M
